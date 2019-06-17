feff55baf10a465533981170105038d695ba378d
Gareth Johnson <garethbjohnson@users.noreply.github.com>
2019-06-17 20:57:26 +0100

    Add working version of HW 1

---
+++ server.js
@@ -0,0 +1,79 @@
+const dotenv = require('dotenv')
+const express = require('express')
+const mongodb = require('mongodb')
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+const uri = process.env.DATABASE_URI
+
+app.get('/api/books', function(request, response) {
+  const client = new mongodb.MongoClient(uri)
+
+  client.connect(function() {
+    const db = client.db('literature')
+    const collection = db.collection('books')
+
+    const searchObject = {}
+
+    if (request.query.title) {
+      searchObject.title = request.query.title
+    }
+
+    if (request.query.author) {
+      searchObject.author = request.query.author
+    }
+
+    collection.find(searchObject).toArray(function(error, books) {
+      response.send(error || books)
+      client.close()
+    })
+  })
+})
+
+app.get('/api/books/:id', function(request, response) {
+  const client = new mongodb.MongoClient(uri)
+
+  let id
+  try {
+    id = new mongodb.ObjectID(request.params.id)
+  } catch (error) {
+    // console.log(error)
+    response.sendStatus(400)
+    return
+  }
+
+  client.connect(function() {
+    const db = client.db('literature')
+    const collection = db.collection('books')
+
+    const searchObject = { _id: id }
+
+    collection.findOne(searchObject, function(error, book) {
+      if (!book) {
+        response.sendStatus(404)
+      } else {
+        response.send(error || book)
+      }
+
+      client.close()
+    })
+  })
+})
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/books/:id', function(request, response) {
+  response.sendFile(__dirname + '/book.html')
+})
+
+app.get('/authors/:name', function(request, response) {
+  response.sendFile(__dirname + '/author.html')
+})
+
+app.listen(port || 3000, function() {
+  console.log(`Running at \`http://localhost:${port}\`...`)
+})
